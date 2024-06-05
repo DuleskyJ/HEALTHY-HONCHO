6eacffd7401cb9b9babdf79ab9d0dbc36d250127
JasielAaron <150881134+JasielAaron@users.noreply.github.com>
2024-06-04 17:43:18 -0700

    updatedseeds

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -9,15 +9,8 @@ const Profile = () => {
   const { profileId } = useParams();
 
   // If there is no `profileId` in the URL as a parameter, execute the `QUERY_ME` query instead for the logged in user's information
-  const { loading, data } = useQuery(
-    profileId ? QUERY_PROFILE : QUERY_ME,
-    {
-      variables: { profileId: profileId },
-    }
-  );
-
-  // Check if data is returning from the `QUERY_ME` query, then the `QUERY_SINGLE_PROFILE` query
-  const profile = data?.me || data?.profile || {};
+  const { loading, data } = useQuery(QUERY_PROFILE);
+  const profile = data?.profile || {};
 
   // Use React Router's `<Navigate />` component to redirect to personal profile page if username is yours
   if (Auth.loggedIn() && Auth.getProfile().data._id === profileId) {
