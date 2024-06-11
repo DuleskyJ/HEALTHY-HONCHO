c8814bf0491c7680428b0d3d6c3f3e231d83f055
Laptop <jdkyaz@gmail.com>
2024-06-11 02:12:12 -0700

    Update MongoDB connection for deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://JadenD:647777@clusterjad.eeqlknz.mongodb.net/healthyhuncho?retryWrites=true&w=majority&appName=ClusterJad');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://JadenD:647777@clusterjad.eeqlknz.mongodb.net/healthyhuncho?retryWrites=true&w=majority&appName=ClusterJad', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 module.exports = mongoose.connection;
