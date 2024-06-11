4922463700d0bc1b432a324c69a3d170df1222b9
Laptop <jdkyaz@gmail.com>
2024-06-11 01:44:51 -0700

    Update MongoDB connection string for Atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://JadenD:647777@clusterjad.eeqlknz.mongodb.net/?retryWrites=true&w=majority&appName=JadenD');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://JadenD:647777@clusterjad.eeqlknz.mongodb.net/?retryWrites=true&w=majority&appName=healthyhoncho');
 
 module.exports = mongoose.connection;
