c85f32265b7093dff4b1f9bd9601fafd90a3919c
Laptop <jdkyaz@gmail.com>
2024-06-11 01:36:48 -0700

    yurr

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/healthy_honcho');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://JadenD:647777@clusterjad.eeqlknz.mongodb.net/?retryWrites=true&w=majority&appName=healthy_huncho');
 
 module.exports = mongoose.connection;
