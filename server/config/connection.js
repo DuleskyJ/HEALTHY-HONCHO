b65ee634fc35f3f1118679675835039638971231
Laptop <jdkyaz@gmail.com>
2024-06-11 06:03:50 -0700

    Reinstall dependencies and clean npm cache

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://JadenD:647777@clusterjad.eeqlknz.mongodb.net/healthyhuncho?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://JadenD:647777@clusterjad.eeqlknz.mongodb.net/?retryWrites=true', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
