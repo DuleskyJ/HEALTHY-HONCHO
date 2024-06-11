5caae7987fec54b677ea09a312e2131379d6a8b7
Laptop <jdkyaz@gmail.com>
2024-06-11 02:21:41 -0700

    Update MongoDB connection string

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://JadenD:647777@clusterjad.eeqlknz.mongodb.net/healthyhuncho?retryWrites=true&w=majority&appName=ClusterJad', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://JadenD:647777@clusterjad.eeqlknz.mongodb.net/healthyhuncho?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
